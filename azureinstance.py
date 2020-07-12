f976e5e1edbdca230e84f3adb2e5e5c22220a41b
rfparedes <rparedes@gmail.com>
2020-07-11 21:47:33 -0400

    Added commenting lines between methods

--- azureinstance.py
+++ azureinstance.py
@@ -9,6 +9,7 @@ import pickle
 
 
 class AzureInstance:
+    """Store Azure instance information"""
 
     def __init__(self, name, provider, region,
                  zone, instance_type, imageid):
@@ -19,59 +20,73 @@ class AzureInstance:
         self._instance_type = instance_type
         self._imageid = imageid
 
+    # --------------------------------------------------------------------
     def __str__(self):
         return 'AWSInstance(name=' + self._instance + ', provider=' + self._provider + ', region=' + self._region + ', zone=' + \
             self._zone + ', type=' + self._instance_type + \
             ', imageid=' + self._imageid + ')'
 
+    # --------------------------------------------------------------------
     def get_instance(self):
         """Return name of instance"""
         return self._instance
 
+    # --------------------------------------------------------------------
     def set_instance(self, instance):
         """Set name of instance"""
         self._instance = instance
 
+    # --------------------------------------------------------------------
     def get_provider(self):
         """Return name of provider"""
         return self._provider
 
+    # --------------------------------------------------------------------
     def set_provider(self, provider):
         """Set name of provider"""
         self._provider = provider
 
+    # --------------------------------------------------------------------
     def get_region(self):
         """Return name of region"""
         return self._region
 
+    # --------------------------------------------------------------------
     def set_region(self, region):
         """Set region of instance"""
         self._region = region
 
+    # --------------------------------------------------------------------
     def get_zone(self):
         """Return name of zone"""
         return self._zone
 
+    # --------------------------------------------------------------------
     def set_zone(self, zone):
         """Set name of zone"""
         self._zone = zone
 
+    # --------------------------------------------------------------------
     def get_instance_type(self):
         """Get the instance type"""
         return self._instance_type
 
+    # --------------------------------------------------------------------
     def set_instance_type(self, instance_type):
         """Set the instance type"""
         self._instance_type = instance_type
 
+    # --------------------------------------------------------------------
     def get_image(self):
         """Return image id"""
         return self._imageid
 
+    # --------------------------------------------------------------------
     def set_ami(self, imageid):
         """Set ami id of instance"""
         self._imageid = imageid
 
+    # --------------------------------------------------------------------
     def get_azure_regions_azs(self):
         """Get regions via Azure SDK"""
         if (not (os.path.isfile(REGION_CACHE_FILENAME + self.get_provider()))):
@@ -94,6 +109,7 @@ class AzureInstance:
                 self.get_provider())
         return sorted(regions)
 
+    # --------------------------------------------------------------------
     def get_all_azure_images(self):
         """Get all images via Azure SDK"""
         """Azure marketplace offer names and skus are nonsensical so it's difficult to programatically determine"""
@@ -152,6 +168,7 @@ class AzureInstance:
                 open(".azureimagecache", "rb"))
             return list_of_images
 
+    # --------------------------------------------------------------------
     def get_azure_images(self, list_of_images, os, sles_or_sap):
         """Get images for specific os"""
         """This code is crap and not general enough because of the inconsistency of our marketplace offer/skus"""
